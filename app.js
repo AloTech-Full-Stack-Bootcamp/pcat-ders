0351f0c5570aeeab94451cb72804e05ab7b5a033
zeynepnurdilek <zna@MacBook-Pro.local>
2021-10-30 13:07:05 +0300

    ejs template

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
-
 const app = express();
+app.use(express.static("public"));
+app.use(function (req, res, next) {
+  console.log("Request:", req.path);
+  next();
+});
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description",
-  };
-  res.send(photo);
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.get("/photo", (req, res) => {
+  res.render("photo");
 });
 
 const port = 3000;
