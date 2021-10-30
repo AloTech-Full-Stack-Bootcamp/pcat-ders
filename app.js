9eac8921435c169ed9db2b38ffcd83b4b65aa48c
zeynepnurdilek <zna@MacBook-Pro.local>
2021-10-30 15:47:37 +0300

    photo create

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+
+const photoController = require('./controller/photoController');
 const app = express();
-app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+
 app.use(function (req, res, next) {
-  console.log("Request:", req.path);
+  console.log('Request:', req.path);
   next();
 });
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-app.get("/add", (req, res) => {
-  res.render("add");
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
-app.get("/photo", (req, res) => {
-  res.render("photo");
+app.get('/photo', (req, res) => {
+  res.render('photo');
+});
+
+app.post('/photo', (req, res) => {
+  const uploadDir = './public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
